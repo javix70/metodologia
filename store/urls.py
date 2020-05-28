4222743a016049d95e878ededa45ca705eeec386
Javier Omar <javieromar.pxl@gmail.com>
2020-05-28 12:34:07 -0400

    first commit

---
+++ store/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from . import views
+
+
+#app_name = store
+
+urlpatterns = [
+    path('', views.store,name="store"),
+    path('cart/', views.cart,name="cart"),
+    path('checkout', views.checkout,name="checkout"),
+]
